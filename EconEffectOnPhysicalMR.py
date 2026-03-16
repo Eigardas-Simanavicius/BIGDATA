aa1c67691f6fdf8a61239e1415719474718e4b5b
Daniil Pokataev <thisisfakeasheck2@gmail.com>
2026-03-16 16:53:02 +0000

    fixed spark implementation to just use basic MapReduce, instead of SparkSQL which is what we were using before.

--- EconEffectOnPhysicalMR.py
+++ EconEffectOnPhysicalMR.py
@@ -1,25 +1,41 @@
 import csv
 import os
 import findspark
+import time
 os.environ["SPARK_HOME"] = "./spark-3.3.1-bin-hadoop3"
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 findspark.init()
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import round,when,col,avg
+from pyspark import SparkConf, SparkContext
 
-# This program will take in a file, a target and the bins you want, and will find the average final gpa of them
-# your targets will be inclsive so if your bin has 25 it will be >= 25 and be put into the 25 bin
-# the last bin will be treated as a "none of the above sections" if done correctly it will be > than largest check, this is done to try to catch all the data
-# and obviosuly your first bin will include anything smaller than it aswell
-def main():
-    #Setting up spark to use MapReduce to process the data
-    spark = SparkSession.builder.master("local[*]").getOrCreate()
-    spark.conf.set("spark.sql.repl.eagerEval.enabled", True) #  This will format our output tables a bit nicer when not using the show() method
-    spark
-    df = spark.read.csv("student_academic_performance_1M.csv", header = True, mode = "DROPMALFORMED",inferSchema=True)
+metric = 33
+bins = []
 
-    intake = int(input("Choose which economic factor's effect on physical conditions to analyse: \n 1. Family Income \n 2. Parent Education \n 3. Part-time Job Hours \n 4. Parent Involvement (*) \n 5. Financial Stress (*) \nOr enter '10' to compare averages for all of the above with different GPAs/Exam Results \n"))
-    bins = [0]
+def get_metric_range(metric_val):
+    if metric_val <= bins[0]:
+        return "0-" + str(bins[0])
+    elif metric_val <= bins[1]:
+        return str(bins[0]) + "-" + str(bins[1])
+    elif metric_val <= bins[2]:
+        return str(bins[1]) + "-" + str(bins[2])
+    elif metric_val <= bins[3]:
+        return str(bins[2]) + "-" + str(bins[3])
+    elif metric_val <= bins[4]:
+        return str(bins[3]) + "-" + str(bins[4])
+    else:
+        return "11+"
+
+# filters a given line down to only the fields we need
+def parse_line(line):
+    fields = line.split(",")
+    measured_metric = float(fields[metric])
+    sleep_qual = float(fields[26])
+    illness_days = float(fields[24])
+    junk_food_freq = float(fields[22])
+    return (measured_metric, sleep_qual, illness_days, junk_food_freq)
+
+
+#grabs the right bins based on the metric we're assessing
+def select_bins(intake):
     if intake == 1:
         bins = [0.2, 0.4, 0.6, 0.8, 1]
     elif intake == 2:
@@ -30,18 +46,62 @@ def main():
         bins = [2, 4, 6, 8, 10]
     elif intake == 5:
         bins = [2, 4, 6, 8, 10]
-    checkList = ["family_income", "parent_education", "part_time_job_hours", "parent_involvement", "financial_stress"]
-    target = checkList[intake-1]
-    dataCheck(target, bins, spark, df)
+    return bins
 
-def dataCheck(target, bins, spark, df):
-    sc = spark.sparkContext
-    average = df.groupBy(
-        when((col(target) >= 0) & (col(target) <= bins[0]), "0-"+str(bins[0]))
-        .when((col(target) > bins[0]) & (col(target) <= bins[1]), str(bins[0]) + "-" + str(bins[1]))
-        .when((col(target) > bins[1]) & (col(target) <= bins[2]), str(bins[1]) + "-" + str(bins[2]))
-        .when((col(target) > bins[2]) & (col(target) <= bins[3]), str(bins[2]) + "-" + str(bins[3]))
-        .when((col(target) > bins[3]) & (col(target) <= bins[4]), str(bins[3]) + "-" + str(bins[4]))
-        .otherwise(str(bins[4]) + "+").alias(target)).avg("sleep_quality", "illness_days", "junk_food_freq").sort().show()
+#finds the "numerical" position of the metric being measured based on its name
+def find_metric(header, metric_name):
+    fields = header.split(",")
+    for x in range(52):
+        if(fields[x] == metric_name):
+            return x
+
+def main():
+    global metric
+    global bins
+
+    intake = int(input("Choose which economic factor's effect on physical conditions to analyse: \n 1. Family Income \n 2. Parent Education \n 3. Part-time Job Hours \n 4. Parent Involvement (*) \n 5. Financial Stress (*)"))
+    checkList = ["family_income", "parent_education", "part_time_job_hours", "parent_involvement", "financial_stress"]
+    metric_name = checkList[intake-1]
+    bins = select_bins(intake)
+
+
+    conf = SparkConf().setAppName("Student Data Analysis")
+    sc = SparkContext(conf=conf)
+
+    # Load CSV
+    data = sc.textFile("student_academic_performance_1M.csv")
+    header = data.first()
+    metric = find_metric(header, metric_name)
+    rows = data.filter(lambda line: line != header)
+
+    start = time.time()
+
+    #parses every row
+    parsed = rows.map(parse_line)
+
+    #uses the Map function to create key value pairs, which look like (metric, (target1, target2, target3, counter))
+    #counter used for averages later
+    key_val_pairs = parsed.map(lambda x: (get_metric_range(x[0]), (x[1], x[2], x[3], 1)))
+
+    #reduces, resulting in tuples of (metric_range, (target1_sum, target2_sum, target3_sum, number of rows in range))
+    range_totals = key_val_pairs.reduceByKey(
+        lambda a, b: (a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3])
+    )
+
+    # calculates average for each range
+    range_averages = range_totals.mapValues(lambda x: (x[0] / x[3], x[1] / x[3], x[2] / x[3]))
+
+    # collects and displays results
+    results = range_averages.collect()
+
+    print(f"\nAverage Sleep Quality, Illness Days, and Junk Food Frequency by {metric_name} Range:")
+    print("-" * 60)
+    for metric_range, averages in sorted(results):
+        avg_sleep, avg_illness, avg_junk_food = averages
+        print(f"{metric_name} Range {metric_range}: Sleep Quality: {avg_sleep:.2f}, Illness Days: {avg_illness:.2f}, Junk Food Frequency: {avg_junk_food:.2f}")
+
+    sc.stop()
+    end = time.time()
+    print(end-start)
 
 main()
